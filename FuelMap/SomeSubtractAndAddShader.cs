30304827676768e4e85095ff57785b5672251dfe
jody sowald <yetyman34@gmail.com>
2021-04-12 19:29:25 -0400

    committing some changes

---
+++ FuelMap/SomeSubtractAndAddShader.cs
@@ -0,0 +1,153 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace FuelMap
+{
+    class SomeSubtractAndAddShader
+    {
+        int Handle;
+        Stopwatch _timer;
+        public int VertexShader { get; private set; }
+        public int FragmentShader { get; private set; }
+        public int FromTextureLocation { get; private set; }
+        public int SubtractTextureLocation { get; private set; }
+        public int FromTexture { get; private set; }
+        public int SubtractTexture { get; private set; }
+        public int AddValueLocation { get; private set; }
+        public float AddValue { get; set; }
+        public SomeSubtractAndAddShader(string vertexPath, string fragmentPath, int fromTexture, int subtractTexture, float addValue)
+        {
+            _timer = new Stopwatch();
+            string VertexShaderSource;
+
+            using (StreamReader reader = new StreamReader(vertexPath, Encoding.UTF8))
+            {
+                VertexShaderSource = reader.ReadToEnd();
+            }
+
+            string FragmentShaderSource;
+
+            using (StreamReader reader = new StreamReader(fragmentPath, Encoding.UTF8))
+            {
+                FragmentShaderSource = reader.ReadToEnd();
+            }
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+            GL.CompileShader(VertexShader);
+
+            string infoLogVert = GL.GetShaderInfoLog(VertexShader);
+            if (infoLogVert != System.String.Empty)
+                System.Console.WriteLine(infoLogVert);
+
+            GL.CompileShader(FragmentShader);
+
+            string infoLogFrag = GL.GetShaderInfoLog(FragmentShader);
+
+            if (infoLogFrag != System.String.Empty)
+                System.Console.WriteLine(infoLogFrag);
+
+            //create a program pointer
+            Handle = GL.CreateProgram();
+
+            //slot in the shaders for this program
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+
+            //compile a program with these settings at this pointer
+            GL.LinkProgram(Handle);
+            //remove the settings, the program is already compiled
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+
+
+
+            FromTextureLocation = GL.GetUniformLocation(Handle, "fromMap");
+            SubtractTextureLocation = GL.GetUniformLocation(Handle, "subtractMap");
+            AddValueLocation = GL.GetUniformLocation(Handle, "addValue");
+            AddValue = addValue;
+            FromTexture = fromTexture;
+            SubtractTexture = subtractTexture;
+
+
+            _timer.Start();
+
+        }
+        public int GetAttribLocation(string attribName)
+        {
+            return GL.GetAttribLocation(Handle, attribName);
+        }
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+            GL.BlendFunc(BlendingFactor.One, BlendingFactor.One);
+            GL.BlendEquation(BlendEquationMode.FuncReverseSubtract);
+            GL.Disable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.Blend);
+            //double timeValue = _timer.Elapsed.TotalSeconds;
+            //float greenValue = (float)Math.Sin(timeValue) / 4.0f + 0.5f;
+
+            CheckGPUErrors("Error setting color:");
+
+
+            GL.ActiveTexture(TextureUnit.Texture0); //select texture unit(hardware) slot
+            GL.BindTexture(TextureTarget.Texture2D, FromTexture); //set the slot to the pointer to texture in gpu memory
+
+            GL.ActiveTexture(TextureUnit.Texture1); //select texture unit(hardware) slot
+            GL.BindTexture(TextureTarget.Texture2D, SubtractTexture); //set the slot to the pointer to texture in gpu memory
+
+            GL.Uniform1(FromTextureLocation, 0);
+            GL.Uniform1(SubtractTextureLocation, 1);
+            GL.Uniform1(AddValueLocation, AddValue);
+
+            CheckGPUErrors("Error setting texture:");
+        }
+
+        private void CheckGPUErrors(string errorPrefix)
+        {
+            ErrorCode err;
+
+            while ((err = GL.GetError()) != ErrorCode.NoError)
+            {
+                //Process/log the error.
+                Console.WriteLine(errorPrefix + err);
+            }
+        }
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        ~SomeSubtractAndAddShader()
+        {
+            GL.DeleteProgram(Handle);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ FuelMap/SomeZeroingShader.cs
@@ -0,0 +1,147 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace FuelMap
+{
+    class SomeZeroingShader
+    {
+        int Handle;
+        Stopwatch _timer;
+        public int VertexShader { get; private set; }
+        public int FragmentShader { get; private set; }
+
+        public int FromTextureLocation { get; private set; }
+        public int FromTexture { get; private set; }
+        public float Average { get; private set; }
+        public SomeZeroingShader(string vertexPath, string fragmentPath, int fromTexture)
+        {
+            _timer = new Stopwatch();
+            string VertexShaderSource;
+
+            using (StreamReader reader = new StreamReader(vertexPath, Encoding.UTF8))
+            {
+                VertexShaderSource = reader.ReadToEnd();
+            }
+
+            string FragmentShaderSource;
+
+            using (StreamReader reader = new StreamReader(fragmentPath, Encoding.UTF8))
+            {
+                FragmentShaderSource = reader.ReadToEnd();
+            }
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+            GL.CompileShader(VertexShader);
+
+            string infoLogVert = GL.GetShaderInfoLog(VertexShader);
+            if (infoLogVert != System.String.Empty)
+                System.Console.WriteLine(infoLogVert);
+
+            GL.CompileShader(FragmentShader);
+
+            string infoLogFrag = GL.GetShaderInfoLog(FragmentShader);
+
+            if (infoLogFrag != System.String.Empty)
+                System.Console.WriteLine(infoLogFrag);
+
+            //create a program pointer
+            Handle = GL.CreateProgram();
+
+            //slot in the shaders for this program
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+
+            //compile a program with these settings at this pointer
+            GL.LinkProgram(Handle);
+            //remove the settings, the program is already compiled
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+
+
+
+            FromTextureLocation = GL.GetUniformLocation(Handle, "fromMap");
+            FromTexture = fromTexture;
+
+
+            _timer.Start();
+
+        }
+        public int GetAttribLocation(string attribName)
+        {
+            return GL.GetAttribLocation(Handle, attribName);
+        }
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+            GL.BlendFunc(BlendingFactor.Zero, BlendingFactor.One);
+            GL.BlendEquation(BlendEquationMode.FuncAdd);
+            GL.Disable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.Blend);
+
+            CheckGPUErrors("Error setting color:");
+
+            GL.ActiveTexture(TextureUnit.Texture0); //select texture unit(hardware) slot
+            GL.BindTexture(TextureTarget.Texture2D, FromTexture); //set the slot to the pointer to texture in gpu memory
+
+            GL.Uniform1(FromTextureLocation, 0);
+
+            CheckGPUErrors("Error setting texture:");
+        }
+        public void CheckAverage(float w, float h)
+        {
+            GL.GenerateTextureMipmap(FromTextureLocation);
+            float fPixel = 0f;
+            GL.BindTexture(TextureTarget.Texture2D, FromTexture);
+            GL.GetTexImage<float>(TextureTarget.Texture2D, (int)(1 + Math.Floor(Math.Log2(Math.Max(w, h)))), PixelFormat.Red, PixelType.Float, ref fPixel);
+            Average = fPixel;//single channel texture
+
+        }
+        private void CheckGPUErrors(string errorPrefix)
+        {
+            ErrorCode err;
+
+            while ((err = GL.GetError()) != ErrorCode.NoError)
+            {
+                //Process/log the error.
+                Console.WriteLine(errorPrefix + err);
+            }
+        }
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        ~SomeZeroingShader()
+        {
+            GL.DeleteProgram(Handle);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
